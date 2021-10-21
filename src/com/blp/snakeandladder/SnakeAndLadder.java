16c455771f7b6211f6a164effdc84d97c826265d
Dhanashree hakke <dhanashreehakke7@gmail.com>
2021-10-21 11:01:15 +0530

    Added: Welcome to Snake and ladder project message is added

---
+++ src/com/blp/snakeandladder/SnakeAndLadder.java
@@ -0,0 +1,12 @@
+package com.blp.snakeandladder;
+
+/**
+ * Snake And Ladder Project
+ */
+
+public class SnakeAndLadder {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Snake And Ladder Project");
+    }
+}
