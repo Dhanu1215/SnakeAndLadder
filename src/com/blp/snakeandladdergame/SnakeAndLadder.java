6c52248f0fe22a0f5d841fd9489e83c126fafdc2
Dhanashree hakke <dhanashreehakke7@gmail.com>
2021-10-21 16:15:27 +0530

    Added: Switch case is used to check player postion

--- src/com/blp/snakeandladdergame/SnakeAndLadder.java
+++ src/com/blp/snakeandladdergame/SnakeAndLadder.java
@@ -4,12 +4,29 @@ package com.blp.snakeandladdergame;
  * Snake And Ladder Game Project
  */
 public class SnakeAndLadder {
+    static int position = 0;
+    public static final int SAME_POSITION = 0;
+    public static final int LADDER = 1;
+    public static final int SNAKE = 2;
 
     public static void main(String[] args) {
-        int position = 0;
-        System.out.println("Position of player = " + position);
+        System.out.println("Starting position of the player = " + position);
         int dice = (int) ((Math.random()*10)%7);
-        System.out.println("Dice number = " + dice);
+        int check = (int) ((Math.random()*10)%3);
+        switch (check) {
+            case SAME_POSITION:
+                System.out.println("Player on stay on same position");
+                break;
+            case LADDER:
+                position = position + dice;
+                System.out.println("Player got ladder and position of the player = " +position);
+            case SNAKE:
+                position = position - dice;
+                if ( position < 0 ) {
+                    position = 0;
+                    System.out.println("Player got snake and position of the player = " +position);
+                }
+        }
     }
 }
 
